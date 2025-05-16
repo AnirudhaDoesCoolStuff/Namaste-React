46db9e43b067a5daba5e22ff4d06e17c1431a8c5
AnirudhaDoesCoolStuff <anirudha.hero@gmail.com>
2025-05-16 10:00:03 +0530

    [2] Igniting the app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const nestedHeading = React.createElement(
   'div',
   { id: 'parent' },
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am h1 tag'),
-    React.createElement('h2', {}, 'I am h2 tag'),
+    React.createElement('h1', { id: 'h1', key: 'heading1' }, 'I am h1 tag'),
+    React.createElement('h2', { id: 'h2', key: 'heading2' }, 'I am h2 tag'),
   ])
 );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(nestedHeading);
